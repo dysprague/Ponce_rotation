55431489f214deb7e5bc5a0fb9a4017c76bcc7b4
Yutaka Sprague <dysprague@yutakas-mbp.harvardsecure.wireless.med.harvard.edu>
2024-10-25 14:29:47 -0400

    fixed code saving

--- inSilico_experiments/gan_inversion/gan_invert_inference.py
+++ inSilico_experiments/gan_inversion/gan_invert_inference.py
@@ -41,6 +41,9 @@ def GAN_inf_movies(G, z_init, z_frames, output_dir):
         image_format = "png"
         img = ToPILImage()(img_opt[0])
         img.save(os.path.join(output_dir, f"{file_name}.{image_format}"))
+        
+        z_save= z_opt.clonse()
+        z_save = z_save.cpu().detach().numpy()
 
         np.save(os.path.join(output_dir, f"{file_name}.npy"), z_use)
 
