4ffce3f9dda816246b61e8f5591c4449c34da219
Yutaka Sprague <dysprague@yutakas-mbp.harvardsecure.wireless.med.harvard.edu>
2024-10-31 17:42:21 -0400

    try running single job to fix OOM issue

--- inSilico_experiments/gan_inversion/imagenet_inv_bash.sh
+++ inSilico_experiments/gan_inversion/imagenet_inv_bash.sh
@@ -7,7 +7,6 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --array=0-3 # change for batches
 #SBATCH --mail-user=daniel_sprague@fas.harvard.edu
 #SBATCH --output=gan_inve_%a.out
 #SBATCH --error=gan_inve_%a.err
@@ -17,4 +16,4 @@ source /n/sw/Miniforge3-24.7.1-0/etc/profile.d/conda.sh
 conda activate ponce_rotation
 
 cd /n/home09/dsprague/Ponce_rotation/inSilico_experiments/gan_inversion
-python3 imagenet_inv.py --task_id $SLURM_ARRAY_TASK_ID 
+python3 imagenet_inv.py --task_id 0
